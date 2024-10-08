77b3b5da029dcb072c82a14ac4dba4fd64b2b6e3
Vans <stevanstc2018@gmail.com>
2024-10-08 13:12:07 +0700

    Initial Commit

---
+++ CloudKitTestProject/CloudKitTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitTestProjectApp.swift
+//  CloudKitTestProject
+//
+//  Created by Stevans Calvin Candra on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
